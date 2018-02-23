43ed29e7bf2b780636e4ca357a1ca9723827c380
Maris Graudins <maris.graudins@mg-software.eu>
2018-02-23 14:21:28 +0200

    JsonFileTarget added.

---
+++ JsonFileTarget.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace mgcode\log;
+
+use yii\helpers\ArrayHelper;
+use yii\log\FileTarget;
+use yii\log\Logger;
+
+/**
+ * Log target that saves logs to json file.
+ *
+ * This log target is inspired by this package: https://github.com/index0h/yii2-log
+ * Some of functions were used and rewritten.
+ */
+class JsonFileTarget extends FileTarget
+{
+    /**
+     * @inheritdoc
+     */
+    public function collect($messages, $final)
+    {
+        $this->messages = array_merge(
+            $this->messages,
+            static::filterMessages($messages, $this->getLevels(), $this->categories, $this->except)
+        );
+        $count = count($this->messages);
+
+        if ($count > 0 && ($final || $this->exportInterval > 0 && $count >= $this->exportInterval)) {
+            $this->addContextToMessages();
+
+            // set exportInterval to 0 to avoid triggering export again while exporting
+            $oldExportInterval = $this->exportInterval;
+            $this->exportInterval = 0;
+            $this->export();
+            $this->exportInterval = $oldExportInterval;
+
+            $this->messages = [];
+        }
+    }
+
+    /**
+     * Formats a log message.
+     * @param array $message The log message to be formatted.
+     * @return string
+     */
+    public function formatMessage($message)
+    {
+        return json_encode($this->prepareMessage($message));
+    }
+
+    /**
+     * Generates the context information to be logged.
+     * @return array
+     */
+    protected function getContextMessage()
+    {
+        $context = [
+            'application' => \Yii::$app->id,
+        ];
+
+        if (($user = \Yii::$app->get('user', false)) !== null) {
+            /** @var \yii\web\User $user */
+            $context['userId'] = $user->getId();
+        }
+        return array_merge($context, ArrayHelper::filter($GLOBALS, $this->logVars));
+    }
+
+    /**
+     * Updates all messages if there are context variables.
+     */
+    protected function addContextToMessages()
+    {
+        $context = $this->getContextMessage();
+        foreach ($this->messages as &$message) {
+            $message[0] = ArrayHelper::merge($this->parseText($message[0]), $context);
+        }
+    }
+
+    /**
+     * Convert's any type of log message to array.
+     * @param mixed $text Input log message.
+     * @return array
+     */
+    protected function parseText($text)
+    {
+        if ($text instanceof \Throwable || $text instanceof \Exception) {
+            return ['@message' => (string) $text];
+        }
+        $type = gettype($text);
+        switch ($type) {
+            case 'array':
+                return $text;
+            case 'string':
+            case 'integer':
+                return ['@message' => $text];
+            case 'object':
+                return get_object_vars($text);
+            default:
+                return ['@message' => \Yii::t('log', "Warning, wrong log message type '{$type}'")];
+        }
+    }
+
+    /**
+     * Transform log message to assoc.
+     * @param array $message The log message.
+     * @return array
+     */
+    protected function prepareMessage($message)
+    {
+        list($text, $level, $category, $timestamp) = $message;
+
+        $level = Logger::getLevelName($level);
+        $timestamp = date('c', $timestamp);
+
+        $result = ArrayHelper::merge(
+            $this->parseText($text),
+            ['level' => $level, 'category' => $category, '@timestamp' => $timestamp]
+        );
+        if (isset($message[4]) === true) {
+            $result['trace'] = $message[4];
+        }
+
+        return $result;
+    }
+}
